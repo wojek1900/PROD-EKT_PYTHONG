a146fb047d96d996a4a64c02929ce2022a0184eb
Antoni Fijałkowski <antek.fijalkowski@gmail.com>
2024-12-22 16:57:44 +0100

    poprawiono ułożenia

--- main.py
+++ main.py
@@ -12,19 +12,34 @@ app = Flask(__name__)
 # opis (przekirowywać do swjej strony)
 # zdjencie profilowe
 ### koniec urzytkownika ###
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY','developerskie')
+app.config['SECURITY_PASSWORD_SALT'] = os.environ.get('SECURITY_PASSWORD_SALT', 'jakas-sol')
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
 db = SQLAlchemy(app)
 
 
+
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = true) #samo się generuje
+    id = db.Column(db.Integer, primary_key = True) #samo się generuje
     nick = db.Column(db.String(100), nullable=False)
     mail = db.Column(db.String(255), nullable=False)
     opis = db.Column(db.String(4096), nullable=True)
-    zdjecie_wskaznik = db.Column(db.Integer, primary_key = true) #bazowo nie ma żadnego i ustawia ikonke podstawową
+    zdjecie_wskaznik = db.Column(db.Integer, primary_key = True) #bazowo nie ma żadnego i ustawia ikonke podstawową
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
