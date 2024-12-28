6d38b01df20175ca176ec3b04d3b3d3cb0a0b95a
Antoni Fijałkowski <antek.fijalkowski@gmail.com>
2024-12-28 19:48:13 +0100

    usuniento zbendy plik

--- models.py
+++
@@ -1,7 +0,0 @@
-class PostAttachment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    post_id = db.Column(db.Integer, db.ForeignKey('public_post.id'), nullable=False)
-    file_name = db.Column(db.String(255), nullable=False)
-    original_filename = db.Column(db.String(255), nullable=False)  # Nowe pole
-    file_path = db.Column(db.String(255), nullable=False)
-    file_type = db.Column(db.String(100))
